6b9fe54db20686044a94e4073b6e46eb1c508078
InputObject <9976114-InputObject@users.noreply.gitlab.com>
2021-12-24 13:30:25 -0500

    Made DNS names no longer bound to specific domain name

--- dns.tf
+++ dns.tf
@@ -1,7 +1,8 @@
 # Configure the DNS Provider
 provider "dns" {
   update {
-    server = var.dns_server
+    server    = var.dns_server
+    transport = "udp"
     gssapi {
       realm    = var.dns_realm
       username = var.dns_username
@@ -14,34 +15,41 @@ provider "dns" {
 # Create a DNS A record set
 resource "dns_a_record_set" "nodes" {
   count     = var.vm_count
-  zone      = "NTMAX.ca."
-  name      = "${xenorchestra_vm.vm[count.index].name_label}.k8s"
+  zone      = var.dns_zone
+  name      = "${xenorchestra_vm.vm[count.index].name_label}.${var.dns_sub_zone}"
   addresses = xenorchestra_vm.vm[count.index].ipv4_addresses
-  ttl = 300
+  ttl       = 900
 }
 
 resource "dns_cname_record" "nodes" {
   count = var.vm_count
-  zone  = "NTMAX.ca."
-  name  = "node-${count.index}.k8s"
-  cname = "${xenorchestra_vm.vm[count.index].name_label}.k8s.NTMAX.ca."
-  ttl = 300
+  zone  = var.dns_zone
+  name  = "node-${count.index}.${var.dns_sub_zone}"
+  cname = "${xenorchestra_vm.vm[count.index].name_label}.${var.dns_sub_zone}.${var.dns_zone}"
+  ttl   = 900
 }
 
 
 # Create a DNS A record set
 resource "dns_a_record_set" "masters" {
   count     = var.master_count
-  zone      = "NTMAX.ca."
-  name      = "${xenorchestra_vm.vm_master[count.index].name_label}.k8s"
+  zone      = var.dns_zone
+  name      = "${xenorchestra_vm.vm_master[count.index].name_label}.${var.dns_sub_zone}"
   addresses = xenorchestra_vm.vm_master[count.index].ipv4_addresses
-  ttl = 300
+  ttl       = 900
 }
 
 resource "dns_cname_record" "masters" {
   count = var.master_count
-  zone  = "NTMAX.ca."
-  name  = "master-${count.index}.k8s"
-  cname = "${xenorchestra_vm.vm_master[count.index].name_label}.k8s.NTMAX.ca."
-  ttl = 300
+  zone  = var.dns_zone
+  name  = "master-${count.index}.${var.dns_sub_zone}"
+  cname = "${xenorchestra_vm.vm_master[count.index].name_label}.${var.dns_sub_zone}.${var.dns_zone}"
+  ttl   = 900
+}
+
+resource "dns_a_record_set" "controlplane" {
+  zone      = var.dns_zone
+  name      = "controlplane.${var.dns_sub_zone}"
+  addresses = xenorchestra_vm.vm_master[*].ipv4_addresses[0]
+  ttl       = 900
 }
